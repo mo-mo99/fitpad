5f76d06ff297c4fe715a039de806c4490490c398
mohamad9999 <mohtashamimohamad99@gmail.com>
2021-05-24 20:10:22 +0300

    'test'

--- fitpad/celery.py
+++ fitpad/celery.py
@@ -1,14 +1,14 @@
-from __future__ import absolute_import, unicode_literals
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTING_MODULE', 'fitpad.setting')
-
-app = Celery('fitpad')
-
-app.config_from_object('django.conf:setting', namespace="CELERY")
-app.autodiscover_tasks()
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+# from __future__ import absolute_import, unicode_literals
+# import os
+# from celery import Celery
+#
+# os.environ.setdefault('DJANGO_SETTING_MODULE', 'fitpad.setting')
+#
+# app = Celery('fitpad')
+#
+# app.config_from_object('django.conf:setting', namespace="CELERY")
+# app.autodiscover_tasks()
+#
+# @app.task(bind=True)
+# def debug_task(self):
+#     print('Request: {0!r}'.format(self.request))
--- fitpad/settings.py
+++ fitpad/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-__a*y-m81*^8&5wh1ihg#xz%wdm#c8n_31w&%=v4r+l69)teu3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['dashboard.heroku.com/apps/django-fitpad', '127.0.0.1']
 
